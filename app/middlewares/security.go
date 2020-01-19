e6d1366bfbf193a4cc6a750c672d1c2b52188e44
chrissie1234 <31969757+chrissie1234@users.noreply.github.com>
2020-01-19 20:33:31 +0100

    Update security.go

--- app/middlewares/security.go
+++ app/middlewares/security.go
@@ -2,7 +2,6 @@ package middlewares
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/getfider/fider/app/pkg/env"
 	"github.com/getfider/fider/app/pkg/web"
@@ -12,12 +11,13 @@ import (
 func Secure() web.MiddlewareFunc {
 	return func(next web.HandlerFunc) web.HandlerFunc {
 		return func(c *web.Context) error {
-			cdnHost := "suggestions.bloxtech.tech"
-			if cdnHost != "" && !env.IsSingleHostMode() {
-				cdnHost = "*." + cdnHost
-			}
-			csp := fmt.Sprintf(web.CspPolicyTemplate, c.ContextID(), cdnHost)
+			//cdnHost := "suggestions.bloxtech.tech"
+			//if cdnHost != "" && !env.IsSingleHostMode() {
+			//	cdnHost = "*." + cdnHost
+			//}
+			//csp := fmt.Sprintf(web.CspPolicyTemplate, c.ContextID(), cdnHost)
 
+	//		c.Response.Header().Set("Content-Security-Policy", strings.TrimSpace(csp))
 			c.Response.Header().Set("X-XSS-Protection", "1; mode=block")
 			c.Response.Header().Set("X-Content-Type-Options", "nosniff")
 			c.Response.Header().Set("Referrer-Policy", "no-referrer-when-downgrade")
